7b89745668170990f8e5f127bfa20def73ec83f2
thelastmedici <christianopeyemijoshua@gmail.com>
2024-10-12 13:03:37 +0100

    added solution to task

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,3 +1,19 @@
+#!/usr/bin/python3
+"""_summary_
+"""
+
+
+def transpose_matrix(matrix, n):
+    """_summary_
+
+    Args:
+                    matrix (_type_): _description_
+    """
+    for i in range(n):
+        for j in range(i, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+
 def reverse_matrix(matrix):
     """_summary_
 
